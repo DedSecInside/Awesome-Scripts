e6fd6e991e54f8c09cd0e9caba2132a5733332a9
sahil <sjha200000@gmail.com>
2018-10-02 21:39:57 +0530

    Added gamil api testing script

---
+++ Gmail_API_script/README.md
@@ -0,0 +1,3 @@
+# A python script to test GMAIL API
+
+make sure you put your api tokens in credentials.json file.
---
+++ Gmail_API_script/main.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+
+def main():
+    
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=creds.authorize(Http()))
+
+  
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
+
+if __name__ == '__main__':
+    main()
