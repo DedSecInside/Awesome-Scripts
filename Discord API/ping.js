924bf03e1f8a48bb8a7f9a832c87e4a22d7395fa
Anuj Attri <anujattri01@gmail.com>
2019-10-10 02:25:18 +0530

    Added features for Javascript
    Signed-off-by: Anuj Attri <anujattri01@gmail.com>

--- Discord API/ping.js
+++ Discord API/ping.js
@@ -1,6 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "!";
+client.on('ready', () => {
+    console.log('I am ready!');
+  client.user.setActivity("<activity>", {type : "<type>"});
+  });                     //displays as 'type
+
 client.on("message", message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
@@ -8,6 +13,11 @@ client.on("message", message => {
         message.reply(`Pong! Client ping is currently ${client.ping}.`);
     }
 });
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find(ch => ch.name === 'general');
+    if (!channel) return;
+    channel.send(`Welcome to the server, ${member}`);
+  });  //Welcomes new members
 
 // Specify token, see readme.
 client.login("<token>");
