5fbee43c06d0ebb6d3f5e13c206ad61249e822d2
Lasith Chandupa <42702535+chandupalasith@users.noreply.github.com>
2020-10-20 20:22:31 +0530

    python_output.py

---
+++ File Transfer/python_output.py
@@ -0,0 +1,40 @@
+"using pyhton as ouput mode"
+
+#when dealing with user inputs, outputs and other operation python provides data types which
+#are very useful in data handling
+
+"it provides 6 data types"
+#1 numbers(int,float)
+#2 string
+#3 bool
+#4 tuple
+#5 dictionary
+#6 list
+
+
+#-----Type Function-------#
+
+"we can check the data type of a value using the type function"
+
+print(type(9))
+#output : <class 'int'>
+"it returns the class where that variable is belong"
+print()
+
+
+
+num=0.5
+print(type(num))
+#output : <class 'float'>
+
+
+
+name = "sajith"
+print(type(name))
+#output : <class 'str'>
+
+
+
+#-----Input Function-------#
+
+"we can get an input form a user by using this function"
