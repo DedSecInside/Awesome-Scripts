6c6b6dbcefd49c9aa24181ea070946154e7a3a07
Mikko Koivunalho <mikko.koivunalho@iki.fi>
2022-10-03 23:16:21 +0200

    Fix whitespace errors
    Cannot run without these corrections:
    
    * Fix tabs to spaces
    * Fix 6 spaces to 8 spaces

--- APIs/Discord API/discord_bot_example.py
+++ APIs/Discord API/discord_bot_example.py
@@ -1,10 +1,10 @@
 #  make sure you have python 3.5.4 or higher
 #  By @ofirisakov
 try:
-	import discord
-	from discord.ext import commands
+        import discord
+        from discord.ext import commands
 except ImportError:
-	print('Please pip install discord')
+        print('Please pip install discord')
 
 
 PREFIX = '!'
@@ -14,25 +14,25 @@ bot = commands.Bot(command_prefix=PREFIX)
 
 @bot.event
 async def on_ready():
-      """
-      Prints on_ready.
+        """
+        Prints on_ready.
 
-      Args:
-      """
-	print('Logged in!')
-	print(f'Name: {bot.user.name}')
-	print(f'ID: {bot.user.id}')
+        Args:
+        """
+        print('Logged in!')
+        print(f'Name: {bot.user.name}')
+        print(f'ID: {bot.user.id}')
 
 
 @bot.command()
 async def ping(ctx):
-      """
-      Send ping message.
+        """
+        Send ping message.
 
-      Args:
-          ctx: (todo): write your description
-      """
-	await ctx.send(f'pong! `{int(bot.latency * 1000)}ms`')
+        Args:
+                ctx: (todo): write your description
+        """
+        await ctx.send(f'pong! `{int(bot.latency * 1000)}ms`')
 
 
 bot.run(TOKEN)
